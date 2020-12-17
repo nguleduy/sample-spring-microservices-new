17d7b449af073f8f5d69eedb9192e5d72747552b
Joseph <nguld@itsj-group.com>
2020-12-17 13:31:01 +0700

    adding feign client

--- department-service/src/main/java/com/example/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/example/departmentservice/DepartmentServiceApplication.java
@@ -5,6 +5,7 @@ import com.example.departmentservice.repository.DepartmentRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -15,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 @EnableSwagger2
 public class DepartmentServiceApplication {
 
--- department-service/src/main/java/com/example/departmentservice/client/EmployeeClient.java
+++ department-service/src/main/java/com/example/departmentservice/client/EmployeeClient.java
@@ -1,11 +1,13 @@
 package com.example.departmentservice.client;
 
 import com.example.departmentservice.model.Employee;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
+@FeignClient(name = "employee-service")
 public interface EmployeeClient {
 
   @GetMapping("/department/{departmentId}")
